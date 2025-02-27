5f8437b86e86cd4e20ceb5d0b960a3b89d85c09b
S4v!0r <gbp17@outlook.com>
2025-02-27 00:53:26 -0500

    first commit

---
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -0,0 +1,30 @@
+import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+
+import { RpcException } from '@nestjs/microservices';
+
+@Catch(RpcException)
+export class RpcCustomExceptionFilter implements ExceptionFilter {
+  catch(exception: RpcException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response: any = ctx.getResponse();
+
+    const rpcError: { status?: number; message?: string } | string =
+      exception.getError();
+
+    if (
+      typeof rpcError === 'object' &&
+      'status' in rpcError &&
+      'message' in rpcError
+    ) {
+      const status = isNaN(+(rpcError as { status: any }).status)
+        ? 400
+        : +(rpcError as { status: any }).status;
+      return response.status(status).json(rpcError);
+    }
+
+    response.status(400).json({
+      status: 400,
+      message: rpcError,
+    });
+  }
+}
---
+++ src/products/products.controller.ts
@@ -0,0 +1,88 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send(
+      { cmd: 'create-product' },
+      createProductDto,
+    );
+  }
+
+  @Get()
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      { cmd: 'find-all-products' },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    // try {
+    //   const product = await firstValueFrom(
+    //     this.productsClient.send({ cmd: 'find-one-product' }, { id }),
+    //   );
+    //   return product;
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
+
+    return this.productsClient.send({ cmd: 'find-one-product' }, { id }).pipe(
+      catchError((error) => {
+        throw new RpcException(
+          typeof error === 'string' ? error : JSON.stringify(error),
+        );
+      }),
+    );
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsClient.send({ cmd: 'remove-product' }, { id }).pipe(
+      catchError((error) => {
+        throw new RpcException(
+          typeof error === 'string' ? error : JSON.stringify(error),
+        );
+      }),
+    );
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsClient
+      .send({ cmd: 'update-product' }, { id, ...updateProductDto })
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(
+            typeof error === 'string' ? error : JSON.stringify(error),
+          );
+        }),
+      );
+  }
+}
