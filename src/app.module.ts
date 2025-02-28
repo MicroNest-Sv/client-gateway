9012bd5177c72333957bbd64ebdce33ab0c70808
S4v!0r <gbp17@outlook.com>
2025-02-28 11:07:33 -0500

    feat: add orders microservice integration with create and update order DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, OrdersModule],
   controllers: [],
   providers: [],
 })
