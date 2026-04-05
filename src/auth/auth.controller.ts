141a32ee5242bed3536ec0620cea4efa542d8c18
s4v!0r <egbp25@outlook.com>
2026-04-04 19:11:36 -0500

    refactor: consolidate DTO imports and enhance error handling type annotations

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,8 +3,8 @@ import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 
 import { NATS_SERVICE } from '@src/config';
-import { RegisterUserDto } from './dto/register-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
+
+import { RegisterUserDto, LoginUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -13,7 +13,7 @@ export class AuthController {
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
     return this.natsClient.send('auth.register', registerUserDto).pipe(
-      catchError((error) => {
+      catchError((error: string | object) => {
         throw new RpcException(error);
       }),
     );
@@ -22,7 +22,7 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.natsClient.send('auth.login', loginUserDto).pipe(
-      catchError((error) => {
+      catchError((error: string | object) => {
         throw new RpcException(error);
       }),
     );
@@ -32,7 +32,7 @@ export class AuthController {
   verify() {
     // TODO: extraer token del header Authorization
     return this.natsClient.send('auth.verify', {}).pipe(
-      catchError((error) => {
+      catchError((error: string | object) => {
         throw new RpcException(error);
       }),
     );
